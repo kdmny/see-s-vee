3805a0d7ee80daee1917bcbef61e77a94d7e7b34
Kale McNaney <kdmny30@gmail.com>
2014-11-27 14:10:20 -0800

    Fix url method

--- app/models/see_s_vee/fun_csv.rb
+++ app/models/see_s_vee/fun_csv.rb
@@ -13,14 +13,10 @@ module SeeSVee
 
     def url
       config = YAML.load(File.open("#{Rails.root}/config/s3.yml"))[Rails.env]
-      AWS::S3::Base.establish_connection!(
-        :access_key_id => config['access_key_id'],
-        :secret_access_key => config['secret_access_key']
-      )
       bucket = config["bucket"]
       key = name
       s3 = AWS::S3.new
-      obj = s3.buckets[bucket].objects["csvs/#{key}"]
+      obj = s3.buckets[bucket].objects["csvs/#{key}.csv"]
       obj.url_for(:read)
     end
   end
