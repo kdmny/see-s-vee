5b298e5a8481a711a551a27c44e811f51578098e
Kale McNaney <kdmny30@gmail.com>
2014-11-27 14:03:25 -0800

    Adding URL method to FunCsv

--- app/models/see_s_vee/fun_csv.rb
+++ app/models/see_s_vee/fun_csv.rb
@@ -1,3 +1,4 @@
+require "aws/s3"
 module SeeSVee
   class FunCsv < ActiveRecord::Base
     attr_accessible :code, :name
@@ -8,6 +9,19 @@ module SeeSVee
     def generate
       set = eval(self.code)
       set.export_csv("csvs/#{self.name}.csv")
+    end
+
+    def url
+      config = YAML.load(File.open("#{Rails.root}/config/s3.yml"))[Rails.env]
+      AWS::S3::Base.establish_connection!(
+        :access_key_id => config['access_key_id'],
+        :secret_access_key => config['secret_access_key']
+      )
+      bucket = config["bucket"]
+      key = name
+      s3 = AWS::S3.new
+      obj = s3.buckets[bucket].objects["csvs/#{key}"]
+      obj.url_for(:read)
     end
   end
 end
